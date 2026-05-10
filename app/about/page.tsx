46a21bb40d79060022cd7c249b7db271d56a164f
Your Name <you@example.com>
2026-05-10 21:20:44 +0530

    Updated project changes

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function About() {
+  return (
+    <div className="flex flex-col w-full min-h-screen">
+      <h1 className="text-4xl font-bold">About Us</h1>
+      <p className="text-lg mt-4">Coming soon...</p>
+    </div>
+  );
+}
